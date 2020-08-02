775e9f2fab4e5f67f274108c057748bdace3ba28
xuwanwan <xuwanwan@outlook.com>
2020-08-03 02:28:59 +0800

    feat:提交

---
+++ node-study/node-todo-xf/index.js
@@ -0,0 +1,11 @@
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
+if (program.debug) console.log(program.opts());
+console.log('pizza details:');
+if (program.small) console.log('- small pizza size');
+if (program.pizzaType) console.log(`- ${program.pizzaType}`);
